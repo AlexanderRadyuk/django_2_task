d2ac9ac9aa6a0f3277cd9b1761d77489887d3e2c
Alexander Radyuk <alexradyuk76@gmail.com>
2024-11-26 20:38:19 +0300

    First commit

---
+++ calculator/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def recipe(request):
+    item_ = request.GET.get('dish')
+    serv = int(request.GET.get('servings'))
+    composition = {}
+    print(DATA[item_].keys())
+    for key_, value_ in DATA.get(item_).items():
+        composition[key_] = round(value_ * serv, 2)
+    print(serv)
+    context = {
+        'recipe': composition,
+    }
+    return render(request,'calculator/index.html', context)
+
+# for key_, value_ in DATA['omlet'].items():
+#     print(key_, value_ * 3)
+
+# def recipe(request):
+#     return HttpResponse('hello')
